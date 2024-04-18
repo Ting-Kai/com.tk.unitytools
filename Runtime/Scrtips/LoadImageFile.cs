7f4ab8bf02eeb8013b6a795a45fb23ebe70b7b83
TK <tk@ultracombos.com>
2024-04-18 13:30:20 +0800

    add LoadImageFile.cs

---
+++ Runtime/Scrtips/LoadImageFile.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadImageFile : MonoBehaviour
+{
+    /*
+    private void OtherScripts()
+    {
+        string path = "";
+        LoadImageFile.Instance.Load(path, tex => {
+
+        });
+    }
+    */
+
+    public static LoadImageFile Instance
+    {
+        get
+        {
+            var component = GameObject.FindAnyObjectByType<LoadImageFile>();
+            if(component == null)
+            {
+                var obj = new GameObject("LoadImageFile (Instance)");
+                component = obj.AddComponent<LoadImageFile>();
+            }
+            return component;
+        }
+    }
+
+    public void Load(string filePath, System.Action<Texture2D> callback)
+    {
+        this.StartCoroutine(LoadFile(filePath,callback));
+    }
+
+    IEnumerator LoadFile(string file, System.Action<Texture2D> callback)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(file);
+
+        yield return request.SendWebRequest();
+
+        Texture2D result = null;
+        if (string.IsNullOrEmpty(request.error))
+        {
+            result = DownloadHandlerTexture.GetContent(request);
+        }
+        else
+        {
+            Debug.Log(request.error);
+        }
+
+        callback?.Invoke(result);
+    }
+}
